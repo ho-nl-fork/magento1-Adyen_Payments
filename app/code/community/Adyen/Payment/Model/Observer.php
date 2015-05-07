ed0999c4689cca1bd361b77ccb9f46de67f3be95
Paul Hachmang <paul@h-o.nl>
2015-05-07 11:28:04 +0200

    Moved available credit cards to config.xml. Still missing some cards.

--- app/code/community/Adyen/Payment/Model/Observer.php
+++ app/code/community/Adyen/Payment/Model/Observer.php
@@ -122,6 +122,8 @@ class Adyen_Payment_Model_Observer {
         $responseData = $this->_getDirectoryLookupResponse($adyFields, $store);
 
         $paymentMethods = array();
+        $ccTypes = array_keys(Mage::helper('adyen')->getCcTypesAltData());
+
         foreach ($responseData['paymentMethods'] as $paymentMethod) {
             $paymentMethod = $this->_fieldMapPaymentMethod($paymentMethod);
             $paymentMethodCode = $paymentMethod['brandCode'];
@@ -133,7 +135,7 @@ class Adyen_Payment_Model_Observer {
             }
 
             if (Mage::getStoreConfigFlag('payment/adyen_cc/active')
-                && in_array($paymentMethodCode, array('diners','discover','amex','mc','visa','maestro'))) {
+                && in_array($paymentMethodCode, $ccTypes)) {
                 continue;
             }
 
